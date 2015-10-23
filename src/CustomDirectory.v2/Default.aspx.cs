d84c052b75e5549deeb5f3d1034562a0b3a8050e
bgiulianetti <bgiulianetti@competir.com>
2015-10-23 01:54:02 -0300

    separacin de codigo en metodos. agregado de keys

--- src/CustomDirectory.v2/Default.aspx.cs
+++ src/CustomDirectory.v2/Default.aspx.cs
@@ -21,66 +21,17 @@ namespace CustomDirectory.v2
             string last = Request.QueryString["l"];
             string pais = Request.QueryString["p"];
             string number = Request.QueryString["n"];
-
-            if (first == null) first = "";
-            if (last == null) last = "";
-            if (pais == null) pais = "";
-            if (number == null) number = "";
+            int start = Int32.Parse(Request.QueryString["start"]);
             #endregion
 
-            //fiddler
-            string url = "", cadena_chile = "", cadena_arg = "", cadena_ambos = "";
+
+            string cadena_chile = "", cadena_argentina = "", cadena_ambos = "";
             bool vacio = false;
-            if (pais.Trim() == string.Empty)
+            if (string.Equals(string.Empty, pais))
             {
-                #region Chile
-                url = "http://10.11.75.10:8080/ccmcip/xmldirectorylist.jsp?l=" + last + "&f=" + first + "&n=" + number;
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                StreamReader sr = new StreamReader(response.GetResponseStream());
-                cadena_chile = sr.ReadToEnd();
-                sr.Close();
-                cadena_chile = cadena_chile.Replace("<?xml version=\"1.0\"?>", "").Replace("<Telephone>", "<Telephone>" + System.Configuration.ConfigurationManager.AppSettings.Get("Prefix_Chile")).Replace("<CiscoIPPhoneDirectory>", "").
-                    Replace("<Name>", "<Name>[CL] ");
-
-                for (int i = 0; i < cadena_chile.Length; i++)
-                    if (cadena_chile[i].ToString() == "<" && cadena_chile[i + 1].ToString() == "P")
-                        cadena_chile = cadena_chile.Substring(0, i);
-
-                int cant = 0;
-                for (int i = 0; i < cadena_chile.Length; i++)
-                {
-                    if (cadena_chile[i].ToString() == "<" && cadena_chile[i + 1].ToString() == "D")
-                        cant++;
-                    if (cant == 16)
-                        cadena_chile = cadena_chile.Substring(0, i);
-                }
-                #endregion
-
-                #region Argentina
-                url = "http://10.11.147.10:8080/ccmcip/xmldirectorylist.jsp?l=" + last + "&f=" + first + "&n=" + number;
-                request = (HttpWebRequest)WebRequest.Create(url);
-                response = (HttpWebResponse)request.GetResponse();
-                sr = new StreamReader(response.GetResponseStream());
-                cadena_arg = sr.ReadToEnd();
-                sr.Close();
-                cadena_arg = cadena_arg.Replace("<?xml version=\"1.0\"?>", "").Replace("<CiscoIPPhoneDirectory>", "").Replace("<Name>", "<Name>[ARG] ").Replace("Garc�a", "Garcia");
-
-                for (int i = 0; i < cadena_arg.Length; i++)
-                    if (cadena_arg[i].ToString() == "<" && cadena_arg[i + 1].ToString() == "P")
-                        cadena_arg = cadena_arg.Substring(0, i);
-
-                cant = 0;
-                for (int i = 0; i < cadena_arg.Length; i++)
-                {
-                    if (cadena_arg[i].ToString() == "<" && cadena_arg[i + 1].ToString() == "D")
-                        cant++;
-                    if (cant == 16)
-                        cadena_arg = cadena_arg.Substring(0, i);
-                }
-                #endregion
-
-                cadena_ambos = cadena_chile + Environment.NewLine + cadena_arg;
+                cadena_chile = GetDirectory("chile", last, first, number, start.ToString());
+                cadena_argentina = GetDirectory("argentina", last, first, number, start.ToString());
+                cadena_ambos = cadena_chile + Environment.NewLine + cadena_argentina;
             }
             else
             {
@@ -98,20 +49,7 @@ namespace CustomDirectory.v2
 
                 if (igual_chile == true || pais.ToLower() == "cl")
                 {
-                    #region Chile
-                    url = "http://10.11.75.10:8080/ccmcip/xmldirectorylist.jsp?l=" + last + "&f=" + first + "&n=" + number;
-                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                    StreamReader sr = new StreamReader(response.GetResponseStream());
-                    cadena_chile = sr.ReadToEnd();
-                    sr.Close();
-                    cadena_chile = cadena_chile.Replace("<?xml version=\"1.0\"?>", "").Replace("<Telephone>", "<Telephone>18562").Replace("<CiscoIPPhoneDirectory>", "").
-                                   Replace("<Name>", "<Name>[CL] ");
-
-                    for (int i = 0; i < cadena_chile.Length; i++)
-                        if (cadena_chile[i].ToString() == "<" && cadena_chile[i + 1].ToString() == "P")
-                            cadena_chile = cadena_chile.Substring(0, i);
-                    #endregion
+                    cadena_chile = GetDirectory("chile", last, first, number, start.ToString());
                 }
                 else
                 {
@@ -129,19 +67,7 @@ namespace CustomDirectory.v2
 
                     if (igual_arg)
                     {
-                        #region Argentina
-                        url = "http://10.11.147.10:8080/ccmcip/xmldirectorylist.jsp?l=" + last + "&f=" + first + "&n=" + number;
-                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                        StreamReader sr = new StreamReader(response.GetResponseStream());
-                        cadena_arg = sr.ReadToEnd();
-                        sr.Close();
-                        cadena_arg = cadena_arg.Replace("<?xml version=\"1.0\"?>", "").Replace("<CiscoIPPhoneDirectory>", "").Replace("<Name>", "<Name>[ARG] ").Replace("Garc�a", "Garcia");
-
-                        for (int i = 0; i < cadena_arg.Length; i++)
-                            if (cadena_arg[i].ToString() == "<" && cadena_arg[i + 1].ToString() == "P")
-                                cadena_arg = cadena_arg.Substring(0, i);
-                        #endregion
+                        cadena_argentina = GetDirectory("argentina", last, first, number, start.ToString());
                     }
                     else
                         vacio = true;
@@ -152,24 +78,79 @@ namespace CustomDirectory.v2
             string final = string.Empty, leyenda = string.Empty;
             if (!vacio)
             {
-                if (cadena_arg != string.Empty && cadena_chile != string.Empty)
-                {
-                    final = cadena_ambos;
-                    leyenda = "Registros de Chile y Argentina";
-                }
-                else if (cadena_arg != string.Empty && cadena_chile == string.Empty)
-                {
-                    final = cadena_arg;
-                    leyenda = "Registros Argentina";
-                }
-                else if (cadena_arg == string.Empty && cadena_chile != string.Empty)
-                {
-                    final = cadena_chile;
-                    leyenda = "Registros Chile";
-                }
+                leyenda = GetCountrySearchCriteriaTitle(cadena_argentina, cadena_chile, cadena_ambos, final);
+                GetMenu(final, leyenda);
+            }
+            else
+                final = "<CiscoIPPhoneDirectory><Prompt>Busqueda sin coincidencias</Prompt></CiscoIPPhoneDirectory>";
 
-                #region Pie de pagina
-                final = "<CiscoIPPhoneDirectory>" + Environment.NewLine + final +
+            Response.ContentType = "text/xml";
+            Response.Write(final);
+        }
+
+
+        private string GetDirectory(string pais, string last, string first, string number, string start)
+        {
+            string url =GetDirectoryUrlByCountry(pais) + last + "&f=" + first + "&n=" + number + "&start=" + start;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string cadena = sr.ReadToEnd();
+            sr.Close();
+
+            cadena = cadena.Replace("<?xml version=\"1.0\"?>", "").Replace("<CiscoIPPhoneDirectory>", "").Replace("<Name>", "<Name>[ARG] ").Replace("Garc�a", "Garcia").Replace("<Telephone>", "<Telephone>" + GetPrefixByCountry(pais));
+
+            for (int i = 0; i < cadena.Length; i++)
+                if (cadena[i].ToString() == "<" && cadena[i + 1].ToString() == "P")
+                    cadena = cadena.Substring(0, i);
+
+            int cant = 0;
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i].ToString() == "<" && cadena[i + 1].ToString() == "D")
+                    cant++;
+                if (cant == 16)
+                    cadena = cadena.Substring(0, i);
+            }
+            return cadena;
+        }
+        private string GetDirectoryUrlByCountry(string country)
+        {
+            if (string.Equals(country, "argentina", StringComparison.InvariantCultureIgnoreCase))
+                return System.Configuration.ConfigurationManager.AppSettings.Get("DirectoryUrlArgentina");
+            else
+                return System.Configuration.ConfigurationManager.AppSettings.Get("DirectoryUrlChile");
+        }
+        private string GetPrefixByCountry(string country)
+        {
+            if (string.Equals(country, "argentina", StringComparison.InvariantCultureIgnoreCase))
+                return System.Configuration.ConfigurationManager.AppSettings.Get("Prefix_Argentina");
+            else
+                return System.Configuration.ConfigurationManager.AppSettings.Get("Prefix_Chile");
+        }
+        private string GetCountrySearchCriteriaTitle(string cadena_argentina, string cadena_chile, string cadena_ambos, string final)
+        {
+            string leyenda = string.Empty;
+            if (cadena_argentina != string.Empty && cadena_chile != string.Empty)
+            {
+                final = cadena_ambos;
+                leyenda = "Registros de Chile y Argentina";
+            }
+            else if (cadena_argentina != string.Empty && cadena_chile == string.Empty)
+            {
+                final = cadena_argentina;
+                leyenda = "Registros Argentina";
+            }
+            else if (cadena_argentina == string.Empty && cadena_chile != string.Empty)
+            {
+                final = cadena_chile;
+                leyenda = "Registros Chile";
+            }
+            return leyenda;
+        }
+        private string GetMenu(string final, string leyenda)
+        {
+            return "<CiscoIPPhoneDirectory>" + Environment.NewLine + final +
                 "<Prompt>" + leyenda + "</Prompt>" + Environment.NewLine +
                 "<SoftKeyItem>" + Environment.NewLine +
                     "<Name>Llamar</Name>" + Environment.NewLine +
@@ -186,14 +167,20 @@ namespace CustomDirectory.v2
                     "<URL>SoftKey:Exit</URL>" +
                     "<Position>3</Position>" + Environment.NewLine +
                 "</SoftKeyItem>" + Environment.NewLine +
-                "</CiscoIPPhoneDirectory>";
-                #endregion
-            }
-            else
-                final = "<CiscoIPPhoneDirectory><Prompt>Busqueda sin coincidencias</Prompt></CiscoIPPhoneDirectory>";
 
-            Response.ContentType = "text/xml";
-            Response.Write(final);
+
+                "<SoftKeyItem>" + Environment.NewLine +
+                "<Name>Next</Name>" + Environment.NewLine +
+                "<URL>" + System.Configuration.ConfigurationManager.AppSettings.Get("SelfUrl") + "?start=32</URL>" + Environment.NewLine +
+                "<Position>4</Position>" + Environment.NewLine +
+                "</SoftKeyItem>" + Environment.NewLine +
+
+                "<SoftKeyItem>" + Environment.NewLine +
+                "<Name>Search</Name>" + Environment.NewLine +
+                "<URL>" + System.Configuration.ConfigurationManager.AppSettings.Get("SelfUrl") + "</URL>" + Environment.NewLine +
+                "<Position>5</Position>" + Environment.NewLine +
+                "</SoftKeyItem>" + Environment.NewLine +
+                "</CiscoIPPhoneDirectory>";
         }
     }
 }
