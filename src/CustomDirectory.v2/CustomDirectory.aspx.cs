014ab5ce106562b6f0d62c012c850751179f07bb
bgiulianetti <bgiulianetti@competir.com>
2015-12-06 18:14:48 -0300

    agregado de transformaciones por Cluester

--- src/CustomDirectory.v2/CustomDirectory.aspx.cs
+++ src/CustomDirectory.v2/CustomDirectory.aspx.cs
@@ -24,7 +24,7 @@ namespace CustomDirectory.v2
             var xmlOutput = string.Empty;
             var language = GetLanguageApplication();
             var first = Request.QueryString["f"];
-            var last = Request.QueryString["l"];
+            var last = "cas";// Request.QueryString["l"];
             var countryCode = Request.QueryString["p"];
             var number = Request.QueryString["n"];
             var start = Request.QueryString["start"];
